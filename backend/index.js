6660852d7f2a04cb5448912c6d182056ba4768db
Chewi9 <hugohg@uma.es>
2026-09-23 17:24:09 +0200

    Cambios

--- backend/index.js
+++ backend/index.js
@@ -17,15 +17,6 @@ app.use('/api/auth', rutasAuth);
 mongoose.connect(process.env.MONGO_URI).then(() => console.log('Conectado a MongoDB')).catch(err => console.error('Erroral conectar a MongoDB', err));
 
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI;
-
-mongoose.connect(MONGO_URI)
-  .then(() => {
-    console.log('Conectado a la base de datos MongoDB');
-    app.listen(PORT, () => {
-      console.log(`Servidor corriendo en http://localhost:${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error('Error conectando a MongoDB:', error.message);
-  });
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
